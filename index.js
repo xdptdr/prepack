a8715ce5b8cdbdca3dfa383edc8b77565d3e1bdf
xavierdpt@gmail.com <xavierdpt@gmail.com>
2017-01-12 13:28:12 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+exports.lodash = require('lodash');
+exports.underscore = require('underscore');
+exports.immutable = require('immutable');
+exports.redux = require('redux');
+exports.reactRedux = require('react-redux');
+exports.reduxThunk = require('redux-thunk');
+exports.xhr = require('xhr');
